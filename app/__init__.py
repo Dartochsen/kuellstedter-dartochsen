c873ebfa632a23058130c1ba51647337ad0649a7
Martin Scheffel <martin@MacBookPro.fritz.box>
2025-02-24 20:31:38 +0100

    Update Firebase initialization to use individual environment variables

--- app/__init__.py
+++ app/__init__.py
@@ -21,8 +21,18 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     # Firebase Initialisierung
-    firebase_service_account = json.loads(os.environ.get('FIREBASE_SERVICE_ACCOUNT_KEY'))
-    cred = credentials.Certificate(firebase_service_account)
+    cred = credentials.Certificate({
+        "type": os.environ.get("FIREBASE_TYPE"),
+        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+        "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL")
+    })
     
     firebase_config = {
         "apiKey": os.environ.get("FIREBASE_API_KEY"),
